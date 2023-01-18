b8cd20befbf097285d9b76027d0abc2a72f6eefc
Faran Bhatti <faran_bhatti@hotmail.com>
2023-01-17 16:23:32 -0800

    Updated UI for final summary page, need to update values with the ones that the user has inputted

--- app/src/main/java/com/example/travelsurvey/Page3.java
+++ app/src/main/java/com/example/travelsurvey/Page3.java
@@ -13,10 +13,12 @@ package com.example.travelsurvey;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Page3 extends AppCompatActivity {
+public class Page3 extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page3);
     }
